2075fdd938a84b2d566081841e2c5a3c1500f032
Viktor Lazarov <viktorlazarov@abv.bg>
2023-08-10 18:23:06 +0200

    added keyboard function

--- index.js
+++ index.js
@@ -1,21 +1,38 @@
 for (let i = 0; i < 7; i++) {
-    document.querySelectorAll("button")[i].addEventListener("click", () => {
-        
-        let btnTxt = document.querySelectorAll("button")[i].innerHTML;
-        let link = "";
-        switch (btnTxt) {
-            case "W": link = "sounds/tom-1.mp3"; break;
-            case "A": link = "sounds/tom-2.mp3"; break;
-            case "S": link = "sounds/tom-3.mp3"; break;
-            case "D": link = "sounds/tom-4.mp3"; break;
-            case "J": link = "sounds/snare.mp3"; break;
-            case "K": link = "sounds/crash.mp3"; break;
-            case "L": link = "sounds/kick-bass.mp3"; break;
-        }
+    // Listens for a button press
+    document.querySelectorAll(".drum")[i].addEventListener("click", () => {
 
-        let audio = new Audio(link)
+        let btnTxt = document.querySelectorAll(".drum")[i].innerHTML;
+        // let buttonInnerHtml = this.innerHTML; ***doesn't work***
+        
+        let audio = new Audio(makeSound(btnTxt))
         audio.play();
 
     });
+}
+// Listens for a keyboard button press
+document.addEventListener("keypress", function (event) {
+    
+    let audio = new Audio(makeSound(event.key.toUpperCase()));
+        audio.play();
+    
+})
+
+// Receives the key or the button that was pressed and 
+// creates a link for the creation of the Audio Object
+function makeSound(text) {
+    
+    let link = "";
+
+    switch (text) {
+        case "W": link = "sounds/tom-1.mp3"; break;
+        case "A": link = "sounds/tom-2.mp3"; break;
+        case "S": link = "sounds/tom-3.mp3"; break;
+        case "D": link = "sounds/tom-4.mp3"; break;
+        case "J": link = "sounds/snare.mp3"; break;
+        case "K": link = "sounds/crash.mp3"; break;
+        case "L": link = "sounds/kick-bass.mp3"; break;
+    }
+    return link;
 }
 
